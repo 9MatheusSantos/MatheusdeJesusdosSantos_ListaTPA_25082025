595acc8a7fc23ae1c11fc492cc83c8f57f58eea2
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 22:57:30 -0300

    Add files via upload

---
+++ exer5/exer5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maior;
+
+        Console.Write("Digite o 1º número: ");
+        maior = int.Parse(Console.ReadLine()); 
+
+        for (int i = 2; i <= 15; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+
+        Console.WriteLine($"\nO maior número digitado foi: {maior}");
+
+        Console.ReadKey();
+    }
+}
