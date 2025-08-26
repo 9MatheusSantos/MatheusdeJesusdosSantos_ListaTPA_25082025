63773e003f65eae9e84d5c208e8a6440b2956df2
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 22:59:06 -0300

    Add files via upload

---
+++ exer7/exer7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write(i);
+
+            if (i % 10 == 0) 
+            {
+                Console.Write(" - MÚLTIPLO DE 10");
+            }
+
+            Console.WriteLine(); 
+        }
+
+        Console.ReadKey();
+    }
+}
