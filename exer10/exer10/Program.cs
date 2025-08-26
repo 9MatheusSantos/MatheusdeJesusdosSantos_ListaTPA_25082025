dd72a999e65c13deaf3abc79dee75600735276e8
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 23:05:37 -0300

    Add files via upload

---
+++ exer10/exer10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Digite o código do operário: ");
+            int C = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de horas trabalhadas: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int salarioNormal = 0;
+            int excedente = 0;
+
+            if (N <= 50)
+            {
+                salarioNormal = N * 10;
+                excedente = 0;
+            }
+            else
+            {
+                salarioNormal = 50 * 10; 
+                excedente = (N - 50) * 20; 
+            }
+
+            int salarioTotal = salarioNormal + excedente;
+
+            Console.WriteLine("\n--- Resultado ---");
+            Console.WriteLine("Salário total: R$ " + salarioTotal);
+            Console.WriteLine("Salário excedente: R$ " + excedente);
+
+
+            Console.Write("\nDeseja encerrar o programa? (S/N): ");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "S")
+            {
+                break;
+            }
+            else
+            {
+                continue; 
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
