6835b4ebf0cb3552e96109f7d2de8cc193618679
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 23:00:07 -0300

    Add files via upload

---
+++ exer9/exer9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int pares = 0, impares = 0, positivos = 0, negativos = 0;
+
+        while (true)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            if (numero % 2 == 0)
+                pares++;
+            else
+                impares++;
+
+
+            if (numero > 0)
+                positivos++;
+            else if (numero < 0)
+                negativos++;
+
+            Console.Write("Deseja encerrar o programa? (S/N): ");
+            string resposta = Console.ReadLine();
+
+
+            if (resposta == "S")
+                break;
+        }
+
+        Console.WriteLine("\n--- Resultado ---");
+        Console.WriteLine($"Quantidade de pares: {pares}");
+        Console.WriteLine($"Quantidade de ímpares: {impares}");
+        Console.WriteLine($"Quantidade de positivos: {positivos}");
+        Console.WriteLine($"Quantidade de negativos: {negativos}");
+
+        Console.ReadKey();
+    }
+}
