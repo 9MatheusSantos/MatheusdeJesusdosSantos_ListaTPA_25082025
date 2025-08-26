5a365c683c0c68e0feca4578b6e1a299f866f687
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 22:55:17 -0300

    Add files via upload

---
+++ exer2/exer2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        int maior, menor;
+
+        if (n1 > n2)
+        {
+            maior = n1;
+            menor = n2;
+        }
+        else
+        {
+            maior = n2;
+            menor = n1;
+        }
+
+        Console.WriteLine($"\nNúmeros ímpares entre {n1} e {n2} (em ordem decrescente):");
+
+        for (int i = maior; i >= menor; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.ReadKey();
+    }
+}
