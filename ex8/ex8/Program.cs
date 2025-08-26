00c69a73af90c4040b8598f894432634c2c4f24f
Matheus <th3th3ug4m3r@gmail.com>
2025-08-25 22:59:42 -0300

    Add files via upload

---
+++ ex8/ex8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um número para calcular seu fatorial.");
+            double n = double.Parse(Console.ReadLine());
+            double total = 1;
+            for (double e = 1; e <= n; e++)
+            {
+                total *= e;
+            }
+            Console.WriteLine($"O fatorial de {n} é igual a: {total}" );
+        }
+    }
+}
